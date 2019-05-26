a4865f7a5f5307ff646b94977786e152af9ad296
Jonas Langford <jonas.langford@protonmail.ch>
2019-05-26 18:47:55 +0200

    [FEATURE] add basic importance handling

---
+++ js/handleImportance.js
@@ -0,0 +1,23 @@
+const importanceContainer = document.querySelector('.note-content-importance');
+
+const importanceInputs = importanceContainer.querySelectorAll("input[name='importance']");
+
+importanceContainer.addEventListener("click", () => {
+    checkedInput = event.target;
+    checkedInputValue = checkedInput.value;
+    console.log(checkedInputValue);
+    removeHighlights(5);
+    loopImportance(checkedInputValue);
+});
+
+function loopImportance(iterations) {
+    for (let i = 0; i < iterations; i++) {
+        importanceInputs[i].closest(".note-content-input-group").classList.add('highlited');
+    }
+}
+
+function removeHighlights(iterations) {
+    for (let i = 0; i < iterations; i++) {
+        importanceInputs[i].closest(".note-content-input-group").classList.remove('highlited');
+    }
+}
