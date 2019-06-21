9397e8b7a5eb062a5f82d02f4d02a39c85a333e3
Jonas Langford <jonas.langford@protonmail.ch>
2019-06-21 12:40:38 +0200

    [FEATURE] adjust orderstore

--- services/orderStore.mjs
+++ services/orderStore.mjs
@@ -1,36 +1,66 @@
 import Datastore from 'nedb-promise'
 
-export class Order {
-    constructor(pizzaName, orderedBy) {
-        this.orderedBy = orderedBy;
-        this.pizzaName = pizzaName;
-        this.orderDate = new Date();
-        this.state = "OK";
+// export class Order {
+//     constructor(pizzaName, orderedBy) {
+//         this.orderedBy = orderedBy;
+//         this.pizzaName = pizzaName;
+//         this.orderDate = new Date();
+//         this.state = "OK";
+//     }
+// }
+
+export class Note {
+    constructor(createDate, title, text, importance, dueDate) {
+        // this.id = id;
+        this.createDate = createDate;
+        this.title      = title;
+        this.text       = text;
+        this.importance = importance;
+        this.dueDate    = dueDate;
+        // this.finishDate = '';
     }
 }
+// export class OrderStore {
+//     constructor(db) {
+//         this.db = db || new Datastore({filename: './data/orders.db', autoload: true});
+//     }
 
-export class OrderStore {
+//     async add(pizzaName) {
+//         let order = new Order(pizzaName, orderedBy);
+//         return await this.db.insert(order);
+//     }
+
+//     async delete(id, currentUser) {
+//         await this.db.update({_id: id, orderedBy: currentUser}, {$set: {"state": "DELETED"}});
+//         return await this.get(id);
+//     }
+
+//     async get(id, currentUser) {
+//         return await this.db.findOne({_id: id, orderedBy : currentUser});
+//     }
+
+//     async all(currentUser) {
+//         return await this.db.cfind({orderedBy : currentUser}).sort({ orderDate: -1 }).exec();
+//     }
+// }
+
+export class NoteStore {
     constructor(db) {
-        this.db = db || new Datastore({filename: './data/orders.db', autoload: true});
+        this.db = db || new Datastore({filename: './data/notes.db', autoload: true});
     }
 
-    async add(pizzaName, orderedBy) {
-        let order = new Order(pizzaName, orderedBy);
-        return await this.db.insert(order);
+    async add(createDate, title, text, importance, dueDate) {
+        let note = new Note(createDate, title, text, importance, dueDate);
+        return await this.db.insert(note);
     }
 
-    async delete(id, currentUser) {
-        await this.db.update({_id: id, orderedBy: currentUser}, {$set: {"state": "DELETED"}});
-        return await this.get(id);
+    async get(id) {
+        return await this.db.findOne({_id: id});
     }
 
-    async get(id, currentUser) {
-        return await this.db.findOne({_id: id, orderedBy : currentUser});
-    }
-
-    async all(currentUser) {
-        return await this.db.cfind({orderedBy : currentUser}).sort({ orderDate: -1 }).exec();
+    async all() {
+        return await this.db.cfind({}).sort({ orderDate: -1 }).exec();
     }
 }
 
-export const orderStore = new OrderStore();
+export const noteStore = new NoteStore();
